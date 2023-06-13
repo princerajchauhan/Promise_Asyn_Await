f82d172cb70f4f7e100287fa1004074fd60ba978
Prince <princeraj.chauhan@outlook.com>
2023-06-13 14:09:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,145 @@
+
+
+// ******************** Question 1 ***********************
+
+let isCode1 = true
+function seeHide1(element) {
+    if (isCode1) {
+        document.getElementById('code1').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode1 = false
+    }
+    else{
+        document.getElementById('code1').style.display = "none"
+        element.innerText = "See Code"
+        isCode1 = true
+    }
+}
+
+// ******************** Question 2 ***********************
+
+let isCode2 = true
+function seeHide2(element) {
+    if (isCode2) {
+        document.getElementById('code2').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode2 = false
+    }
+    else{
+        document.getElementById('code2').style.display = "none"
+        element.innerText = "See Code"
+        isCode2 = true
+    }
+}
+
+// ******************** Question 3 ***********************
+
+let isCode3 = true
+function seeHide3(element) {
+    if (isCode3) {
+        document.getElementById('code3').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode3 = false
+    }
+    else{
+        document.getElementById('code3').style.display = "none"
+        element.innerText = "See Code"
+        isCode3 = true
+    }
+}
+
+// ******************** Question 4 ***********************
+
+let isCode4 = true
+function seeHide4(element) {
+    if (isCode4) {
+        document.getElementById('code4').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode4 = false
+    }
+    else{
+        document.getElementById('code4').style.display = "none"
+        element.innerText = "See Code"
+        isCode4 = true
+    }
+}
+
+// ******************** Question 5 ***********************
+
+let isCode5 = true
+function seeHide5(element) {
+    if (isCode5) {
+        document.getElementById('code5').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode5 = false
+    }
+    else{
+        document.getElementById('code5').style.display = "none"
+        element.innerText = "See Code"
+        isCode5 = true
+    }
+}
+
+// ******************** Question 6 ***********************
+
+let isCode6 = true
+function seeHide6(element) {
+    if (isCode6) {
+        document.getElementById('code6').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode6 = false
+    }
+    else{
+        document.getElementById('code6').style.display = "none"
+        element.innerText = "See Code"
+        isCode6 = true
+    }
+}
+
+// ******************** Question 7 ***********************
+
+let isCode7 = true
+function seeHide7(element) {
+    if (isCode7) {
+        document.getElementById('code7').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode7 = false
+    }
+    else{
+        document.getElementById('code7').style.display = "none"
+        element.innerText = "See Code"
+        isCode7 = true
+    }
+}
+
+// ******************** Question 8 ***********************
+
+let isCode8 = true
+function seeHide8(element) {
+    if (isCode8) {
+        document.getElementById('code8').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode8 = false
+    }
+    else{
+        document.getElementById('code8').style.display = "none"
+        element.innerText = "See Code"
+        isCode8 = true
+    }
+}
+
+// ******************** Question 9 ***********************
+
+let isCode9 = true
+function seeHide9(element) {
+    if (isCode9) {
+        document.getElementById('code9').style.display = "block"
+        element.innerText = "Hide Code"
+        isCode9 = false
+    }
+    else{
+        document.getElementById('code9').style.display = "none"
+        element.innerText = "See Code"
+        isCode9 = true
+    }
+}
